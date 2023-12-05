974bf7b8d759a0f07de91f795336a5b27175cd91
mb4c <128654806+mb4c@users.noreply.github.com>
2023-12-05 12:50:25 +0100

    Add singleton class template

---
+++ RVEngine/include/Singleton.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+template<typename T>
+class Singleton
+{
+public:
+	static T& instance();
+
+	Singleton(const Singleton&) = delete;
+
+	Singleton& operator=(const Singleton) = delete;
+
+protected:
+	struct token
+	{
+	};
+
+	Singleton()
+	{}
+};
+
+#include <memory>
+
+template<typename T>
+T& Singleton<T>::instance()
+{
+	static T instance{token{}};
+	return instance;
+}
+
