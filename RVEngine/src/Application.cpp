05f408030aacba6f7fa03b206492eab3c2925c8c
mb4c <128654806+mb4c@users.noreply.github.com>
2024-04-24 12:21:47 +0200

    Revert "Move ImGui render code after update"

--- RVEngine/src/Application.cpp
+++ RVEngine/src/Application.cpp
@@ -100,11 +100,6 @@ void Application::Run()
 		Renderer::Clear();
 
 		{
-			RV_PROFILE_SCOPE("OnUpdate");
-			OnUpdate();
-		}
-
-		{
 			RV_PROFILE_SCOPE("OnImGuiRender");
 			ImGui_ImplOpenGL3_NewFrame();
 			ImGui_ImplGlfw_NewFrame();
@@ -116,6 +111,12 @@ void Application::Run()
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		}
+
+		{
+			RV_PROFILE_SCOPE("OnUpdate");
+			OnUpdate();
+		}
+
 
 		{
 			RV_PROFILE_SCOPE("Swap Buffers");
