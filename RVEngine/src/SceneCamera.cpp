4687b49fa727b5d8e92c08b1b113d40da0a3ad37
mb4c <128654806+mb4c@users.noreply.github.com>
2024-07-26 15:56:03 +0200

    Fix formatting in SceneCamera.cpp

--- RVEngine/src/SceneCamera.cpp
+++ RVEngine/src/SceneCamera.cpp
@@ -43,8 +43,7 @@ void SceneCamera::RecalculateProjection()
 		float orthoBottom = -m_OrthographicSize * 0.5f;
 		float orthoTop = m_OrthographicSize * 0.5f;
 
-		m_Projection = glm::ortho(orthoLeft, orthoRight,
-								  orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
+		m_Projection = glm::ortho(orthoLeft, orthoRight, orthoBottom, orthoTop, m_OrthographicNear, m_OrthographicFar);
 	}
 
 }
