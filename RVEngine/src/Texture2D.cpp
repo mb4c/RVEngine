a5792a3fb104782f89258f5bd75293f0497008f9
mb4c <marcin@marcinbac.pl>
2024-08-13 02:07:57 +0200

    Cleanup Texture2D creation from color

--- RVEngine/src/Texture2D.cpp
+++ RVEngine/src/Texture2D.cpp
@@ -52,8 +52,6 @@ Texture2D::Texture2D(uint32_t width, uint32_t height, glm::vec4 color, bool norm
 {
 	RV_PROFILE_FUNCTION();
 
-	unsigned int textureID;
-	glGenTextures(1, &textureID);
 	unsigned char pixelData[width * height * 4];
 
 	for (int i = 0; i < width * height; ++i)
@@ -64,26 +62,8 @@ Texture2D::Texture2D(uint32_t width, uint32_t height, glm::vec4 color, bool norm
 		pixelData[i * 4 + 3] = static_cast<unsigned char>(color.a * 255);
 	}
 
-	auto format = GL_RGBA;
-
-	glBindTexture(GL_TEXTURE_2D, textureID);
-	glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, pixelData);
-	glGenerateMipmap(GL_TEXTURE_2D);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
-	if (nearestFiltering)
-	{
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	} else
-	{
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	}
-
-
-	m_ID = textureID;
+	auto tex = Texture2D(width, height, 4, pixelData);
+	m_ID = tex.GetTexture();
 }
 
 void Texture2D::Serialize(const std::filesystem::path& file)
